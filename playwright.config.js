d6b3ba6148d69a5eb75eae139d4ae74322149613
Netanel Harush <netanelharush2@gmail.com>
2025-10-15 19:36:29 +0300

    Format Playwright configuration file for improved readability and consistency

--- playwright.config.js
+++ playwright.config.js
@@ -1,30 +1,30 @@
-import {defineConfig, devices} from '@playwright/test'
+import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
-  testDir: './src/tests',
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 2 : undefined,
-  reporter: [['html', {open: 'never'}], ['list']],
-  use: {
-    trace: 'on',
-    screenshot: {mode: 'only-on-failure', fullPage: true},
-    video: 'retain-on-failure',
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: {...devices['Desktop Chrome']},
-    },
+	testDir: "./src/tests",
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	workers: process.env.CI ? 2 : undefined,
+	reporter: [["html", { open: "never" }], ["list"]],
+	use: {
+		trace: "on",
+		screenshot: { mode: "only-on-failure", fullPage: true },
+		video: "retain-on-failure",
+	},
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
 
-    {
-      name: 'firefox',
-      use: {...devices['Desktop Firefox']},
-    },
+		{
+			name: "firefox",
+			use: { ...devices["Desktop Firefox"] },
+		},
 
-    {
-      name: 'webkit',
-      use: {...devices['Desktop Safari']},
-    },
-  ],
-})
+		{
+			name: "webkit",
+			use: { ...devices["Desktop Safari"] },
+		},
+	],
+});
